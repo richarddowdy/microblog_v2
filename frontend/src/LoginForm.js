5a429419bef5653db18224721fb75faa63a4e528
Richard Dowdy <richard.dowdy0@gmail.com>
2021-04-20 23:33:52 -0700

    debug log

--- frontend/src/LoginForm.js
+++ frontend/src/LoginForm.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { BASE_API_URL, userLoginToApi, userSignUpToApi } from './actionCreators'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_API_URL, userLoginToApi, userSignUpToApi } from "./actionCreators";
 // import { authenticate } from "../../backend/models/usersModel";
-import { useHistory } from 'react-router-dom';
-import Axios from 'axios';
-import { decode } from 'jsonwebtoken';
-import { loginUser } from './actions/userActions';
-import { authError } from './actions/errorActions';
+import { useHistory } from "react-router-dom";
+import Axios from "axios";
+import { decode } from "jsonwebtoken";
+import { loginUser } from "./actions/userActions";
+import { authError } from "./actions/errorActions";
 
-function LoginForm({ login, signUp}) {
+function LoginForm({ login, signUp }) {
   const dispatch = useDispatch();
   const history = useHistory();
   const errorMessage = useSelector((st) => st.errors.loginError);
@@ -32,7 +32,7 @@ function LoginForm({ login, signUp}) {
     evt.preventDefault();
     console.log("clicked login/signup");
     let authType = signUp ? "users" : "login";
-    try{
+    try {
       dispatch(authError(""));
       let res = await Axios.post(`${BASE_API_URL}/${authType}`, formData);
       console.log("NEW LOGIN RESPONSE", res);
@@ -40,19 +40,16 @@ function LoginForm({ login, signUp}) {
       const user = decode(token);
       localStorage.setItem("_token", token);
       dispatch(loginUser(user));
-      history.push("/")// TODO: On successful login
-    } catch(err) {
-      dispatch(authError(err.response.data.message))
+      history.push("/"); // TODO: On successful login
+    } catch (err) {
+      dispatch(authError(err.response.data.message));
     }
   };
 
   return (
     <>
-      { errorMessage ? <div className="alert alert-danger">{errorMessage}</div> : null }
-      <form
-        className="col-lg-4 col-md-6 col-8 mt-3 mx-auto"
-        onSubmit={handleSubmit}
-      >
+      {errorMessage ? <div className="alert alert-danger">{errorMessage}</div> : null}
+      <form className="col-lg-4 col-md-6 col-8 mt-3 mx-auto" onSubmit={handleSubmit}>
         <label>Username:</label>
         <input
           className="form-control"
