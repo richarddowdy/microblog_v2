5417335c83e711f8454ebcc204861372c25e203e
Richard Dowdy <richard.dowdy0@gmail.com>
2021-05-05 21:09:42 -0700

    fix

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -8,13 +8,13 @@ function NavBar() {
   
   const currentUser = useSelector((st) => st.user);
   const dispatch = useDispatch();
-
+  
   // TODO Fix this unresponsive mess - mobile looks terrible
   return (
     <div style={{height: "200px"}} className="navbar text-dark">
       <NavLink to="/" className=" app-brand text-dark">Microblog</NavLink>
       <a href="https://github.com/richarddowdy/microblog_v2" target="_blank" rel="noopener noreferrer" className="text-dark text-center"><i className="fab fa-github fa-3x"></i><span className="d-block">Github Repo</span></a>
-      <div style={{display: "block"}} className="text-light col-12-lg">
+      <div style={{display: "block"}} className="text-light col-12">
         <NavLink to="/" className="mr-4 font-weight-bold navbar-link">Blog</NavLink>
         <NavLink to="/new" className="mr-4 font-weight-bold navbar-link">Add a new post</NavLink>
         {currentUser.id ?   
