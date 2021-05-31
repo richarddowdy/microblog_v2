67141a2f012fa8e4c1d855fae675641d7d060316
Richard Dowdy <richard.dowdy0@gmail.com>
2021-05-30 22:59:51 -0700

    add router to app.js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const router = new express.Router();
 const postsRoutes = require("./routes/posts");
 const postCommentsRoutes = require("./routes/postComments");
 const usersRoutes = require("./routes/users");
