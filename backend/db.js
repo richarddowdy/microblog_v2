36dca4b5be5611f5195e5d8a0a826d25bdbef106
Richard Dowdy <richard.dowdy0@gmail.com>
2021-05-31 01:38:55 -0700

    add ssl=true to db new Client

--- backend/db.js
+++ backend/db.js
@@ -5,19 +5,17 @@ const { DB_URI } = require("./config");
 
 // const client = new Client(process.env.DATABASE_URL || "postgresql:///microblog");
 const db = new Client({
-  connectionString: DB_URI
+  connectionString: DB_URI,
+  ssl: true,
 });
 
 db.connect();
 
-
 module.exports = db;
 
-
-
 /**
- * 
+ *
  * CREATE CONFIG FILE SIMILAR TO JOBLY
- * 
+ *
  * UPDATE INFO HERE AS WELL
  */
