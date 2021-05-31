b25b47bfe0c888232b6914b2d018bacb4da7dadd
Richard Dowdy <richard.dowdy0@gmail.com>
2021-05-31 14:22:17 -0700

    change express static path

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "../../frontend", "build", "index.html"));
   });
   // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve("../../frontend/build")));
+  router.use(express.static(path.resolve("frontend/build")));
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
