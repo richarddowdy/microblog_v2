2b2317a858bcd68ab000cdf9ffb01f0530fadba4
Richard Dowdy <richard.dowdy0@gmail.com>
2021-06-02 23:04:27 -0700

    cleanup app.js and actioncreators in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,37 @@
 import React, { useEffect } from "react";
-import { ToastContainer, toast } from "react-toastify";
 import Navbar from "./Navbar";
 import Routes from "./Routes";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
 import { useSelector, useDispatch } from "react-redux";
-import useLocalStorage from "./hooks/useLocalStorage";
 import { decode } from "jsonwebtoken";
-import { getCurrentUserFromApi } from "./actionCreators";
-import { currentUser } from "./actions/userActions";
+import { getCurrentUserFromApi, logoutUser } from "./actionCreators";
 import "react-toastify/dist/ReactToastify.min.css";
 
 export let tokenDefault = "help";
 
 let token = localStorage.getItem("_token");
 function App() {
-  // const [ token, setToken ] = useLocalStorage(tokenDefault);
   const dispatch = useDispatch();
   const store = useSelector((st) => st);
   let currentUser = useSelector((st) => st.user);
-  // console.log("currentUser", currentUser);
-  // console.log("entire store", store);
 
-  // let currentUser;
-  // console.log(token);
   useEffect(() => {
-    // console.log("effect Running");
     async function getCurrentUser() {
       // console.log("entire store", store);
       try {
-        // let tokenInfo = decode(token);
-        // let { is_admin, username } = decode(token);
-        // console.log("current User", username);// TODO: complete persistance of current user
-        // console.log("entire token", tokenInfo);// TODO: complete persistance of current user
         dispatch(getCurrentUserFromApi(token)); //TODO
         // console.log(tokenUser);
-        // dispatch(currentUser({...tokenUser, is_admin}));
+        // dispatch(currentUser({...tokenUser, is_admin})); // TODO someday maybe?
       } catch (err) {
-        // console.log("no token");
-        // dispatch(removeCurrentUser());
+        console.error("no token");
+        dispatch(logoutUser());
       }
-      //   dispatch(loadComplete(true));
     }
-    // dispatch(loadComplete(false));
     if (!currentUser.username) {
       // console.log("fetching current user");
       getCurrentUser();
-      // dispatch(currentUser({}))
     }
-    // getCurrentUser();
     // console.log("theres a user", currentUser);
   }, [token]);
 
