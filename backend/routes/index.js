1a2b87de092b43c5d09b18a8f92a89fb86f9b6b9
Richard Dowdy <richard.dowdy0@gmail.com>
2021-06-02 20:37:07 -0700

    that didn't work

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -20,10 +20,10 @@ if (process.env.NODE_ENV === "production") {
   router.use(express.static(path.resolve("frontend/build")));
   // Serve the frontend's index.html file at all other routes NOT starting with /api
 
-  // router.get(/^(?!\/?api).*/, (req, res) => {
-  //   // res.cookie("XSRF-TOKEN", req.csrfToken());
-  //   res.sendFile(path.resolve(__dirname, "../../frontend", "build", "index.html"));
-  // });
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    // res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.sendFile(path.resolve(__dirname, "../../frontend", "build", "index.html"));
+  });
 }
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== "production") {
