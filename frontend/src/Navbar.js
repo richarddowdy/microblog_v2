0c5a111e7d01e00308b4fe43034619d98e7e7833
Richard Dowdy <richard.dowdy0@gmail.com>
2021-06-07 23:56:38 -0700

    nav links look better

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,34 +1,52 @@
-import React, { useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
-import './Navbar.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { logoutUser } from './actionCreators';
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+import { useSelector, useDispatch } from "react-redux";
+import { logoutUser } from "./actionCreators";
 
 function NavBar() {
-  
   const currentUser = useSelector((st) => st.user);
   const dispatch = useDispatch();
-  
-  // TODO Fix this unresponsive mess - mobile looks terrible
+
   return (
-    <div style={{height: "200px"}} className="navbar text-dark">
-      <NavLink to="/" className=" app-brand text-dark">Microblog</NavLink>
-      <a href="https://github.com/richarddowdy/microblog_v2" target="_blank" rel="noopener noreferrer" className="text-dark text-center"><i className="fab fa-github fa-3x"></i><span className="d-block">Github Repo</span></a>
-      <div style={{display: "block"}} className="text-light col-12">
-        <NavLink to="/" className="mr-4 font-weight-bold navbar-link">Blog</NavLink>
-        <NavLink to="/new" className="mr-4 font-weight-bold navbar-link">Add a new post</NavLink>
-        {currentUser.id ?   
-        <>
-          <NavLink to={`/user/${currentUser.id}`} className="mr-4 font-weight-bold navbar-link" >Profile</NavLink>
-          <NavLink to="/" className="font-weight-bold navbar-link" onClick={() => dispatch(logoutUser())}>Logout</NavLink>
-        </>
-        :
-        <NavLink to="/login" className="font-weight-bold navbar-link">Login / Sign UP</NavLink>
-        }
+    <nav style={{ height: "200px" }} className="navbar">
+      <NavLink to="/" className="app-brand">
+        Microblog
+      </NavLink>
+      <a
+        href="https://github.com/richarddowdy/microblog_v2"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-center"
+        style={{ color: "black" }}
+      >
+        <i className="fab fa-github fa-3x"></i>
+        <span className="d-block">Github Repo</span>
+      </a>
+      <div style={{ display: "block" }} className="text-light col-12">
+        <NavLink to="/" className="mr-4 font-weight-bold navbar-link">
+          Blog
+        </NavLink>
+        <NavLink to="/new" className="mr-4 font-weight-bold navbar-link">
+          Add New post
+        </NavLink>
+        {currentUser.id ? (
+          <>
+            <NavLink to={`/user/${currentUser.id}`} className="mr-4 font-weight-bold navbar-link">
+              Profile
+            </NavLink>
+            <NavLink to="/" className="font-weight-bold navbar-link" onClick={() => dispatch(logoutUser())}>
+              Logout
+            </NavLink>
+          </>
+        ) : (
+          <NavLink to="/login" className="font-weight-bold navbar-link">
+            Login / Sign UP
+          </NavLink>
+        )}
       </div>
-    </div>
+    </nav>
   );
 }
 
 export default NavBar;
-
